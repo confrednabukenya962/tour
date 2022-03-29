c5e163fff205371a7368bfa8cc87d529225c0c33
confrednabukenya962 <confrednabukenya@gmail.com>
2022-03-28 17:38:46 -0700

    first app

--- lib/airlinesflights.dart
+++ lib/airlinesflights.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AirlinesFlights extends StatefulWidget {
   const AirlinesFlights({Key? key}) : super(key: key);
@@ -27,235 +28,207 @@ class _AirlinesFlightsState extends State<AirlinesFlights> {
 
         body: Padding(
           padding:const EdgeInsets.fromLTRB(20.0,15.0,20.0,15.0),
-          child: Container(
-            child: GridView(children: [
+          child: GridView(children: [
               InkWell(
-                onTap: (){
-                  //Navigator.push(context,MaterialPageRoute(builder: (context)=>const SplashScreen()));
-                },
-                child: Container(
-                  decoration:const BoxDecoration(  color:Color(0xffbdbdbd),
-                    border: Border(bottom: BorderSide(
-                    color: Colors.lightBlue,
-                    width: 2,
-                  ),
-                  ),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      //Image.asset("images/tourist.jpg",height: 35,),const SizedBox(height: 25,),
-                      Text("Uganda Airlines",style: TextStyle(fontWeight: FontWeight.bold,
-                          color: Colors.black, fontSize: 12),),
+                onTap: ()=> launch('https://www.ugandairlines.com/'),
+
+                  child: Stack(
+                    //mainAxisAlignment: MainAxisAlignment.center,
+                    children:  [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(15.0),
+                        child:Image.asset("images/ai2.jpg",height: 250,width:250,fit: BoxFit.cover,),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text("Uganda Airlines",style: TextStyle(fontWeight: FontWeight.bold,
+                            color: Colors.black, fontSize: 12),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
 
                     ],
                   ),
                 ),
-              ),
+
               InkWell(
-                onTap: (){
-                  //Navigator.push(context,MaterialPageRoute(builder: (context)=>const TourRecommenderPage()));
-                },
-                child: Container(
-                  decoration:const BoxDecoration(  color:Color(0xffbdbdbd),
-                    border: Border(bottom: BorderSide(
-                    color: Colors.lightBlue,
-                    width: 2,
-                  ),
-                  ),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      //Image.asset("images/tourist.jpg",height: 35,),const SizedBox(height: 25,),
-                      Text("Emirates",style: TextStyle(fontWeight: FontWeight.bold,
-                          color: Colors.black, fontSize: 12),),
+                onTap:  ()=> launch('https://www.emirates.com/ug/english/'),
+
+                child:Stack(
+                   // mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(15.0),
+                        child:Image.asset("images/em7.jpg",height: 250,width:250,fit: BoxFit.cover,),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text("Emirates",style: TextStyle(fontWeight: FontWeight.bold,
+                            color: Colors.black, fontSize: 12),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
                     ],
                   ),
                 ),
-              ),
+
               InkWell(
-                onTap: (){
-                  //Navigator.push(context,MaterialPageRoute(builder: (context)=>const TourOperators()));
-                },
-                child: Container(
-                  decoration:const BoxDecoration(  color:Color(0xffbdbdbd),
-                    border: Border(bottom: BorderSide(
-                    color: Colors.lightBlue,
-                    width: 2,
-                  ),
-                  ),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                     // Image.asset("images/tourist.jpg",height: 35,),const SizedBox(height: 25,),
-                      Text("Kenya Airways",style:  TextStyle(fontWeight: FontWeight.bold,
-                          color: Colors.black, fontSize: 12),),
+                onTap: ()=> launch('https://www.kenya-airways.com/ug/'),
+                child: Stack(
+                    //mainAxisAlignment: MainAxisAlignment.center,
+                    children:[
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(15.0),
+                        child:Image.asset("images/ke6.jpg",height: 250,width:250,fit: BoxFit.cover,),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text("Kenya Airways",style:  TextStyle(fontWeight: FontWeight.bold,
+                            color: Colors.black, fontSize: 12),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
                     ],
                   ),
                 ),
-              ),
+
               InkWell(
-                onTap: (){
-                  //Navigator.push(context,MaterialPageRoute(builder: (context)=>const Lodging()));
-                },
-                child: Container(
-                  decoration:const BoxDecoration(  color:Color(0xffbdbdbd),
-                    border: Border(bottom: BorderSide(
-                      color: Colors.lightBlue,
-                      width: 2,
-                    ),
-                    ),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                     // Image.asset("images/tourist.jpg",height: 35,),const SizedBox(height: 25,),
-                      Text("Royal Dutch Airlines",style: TextStyle(fontWeight: FontWeight.bold,
-                          color: Colors.black, fontSize: 12),),
+                onTap: ()=> launch('https://www.klm.com/'),
+                child: Stack(
+                    //mainAxisAlignment: MainAxisAlignment.center,
+                    children:[
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(15.0),
+                        child:Image.asset("images/rda3.jpg",height: 250,width:250,fit: BoxFit.cover,),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text("Royal Dutch Airlines",style: TextStyle(fontWeight: FontWeight.bold,
+                            color: Colors.black, fontSize: 12),
+                          textAlign: TextAlign.center,),
+                      ),
                     ],
                   ),
                 ),
-              ),
+
               InkWell(
-                onTap: (){
-                  //Navigator.push(context,MaterialPageRoute(builder: (context)=>const PasswordReset()));
-                },
-                child: Container(
-                  decoration:const BoxDecoration(  color:Color(0xffbdbdbd),
-                    border: Border(bottom: BorderSide(
-                      color: Colors.lightBlue,
-                      width: 2,
-                    ),
-                    ),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      //Image.asset("images/tourist.jpg",height: 35,),const SizedBox(height: 25,),
-                      Text("Fly Dubai",style: TextStyle(fontWeight: FontWeight.bold,
-                          color: Colors.black, fontSize: 12),),
+                onTap: ()=> launch('https://www.flydubai.com/en-ae/flights-from-uganda/'),
+                child: Stack(
+                    //mainAxisAlignment: MainAxisAlignment.center,
+                    children:[
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(15.0),
+                        child:Image.asset("images/fd1.jpg",height: 250,width:250,fit: BoxFit.cover,),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text("Fly Dubai",style: TextStyle(fontWeight: FontWeight.bold,
+                            color: Colors.black, fontSize: 12),
+                          textAlign: TextAlign.center,),
+                      ),
                     ],
                   ),
                 ),
-              ),
+
               InkWell(
-                onTap: (){
-                  //Navigator.push(context,MaterialPageRoute(builder: (context)=>const AirportTransport()));
-                },
-                child: Container(
-                  decoration:const BoxDecoration(  color:Color(0xffbdbdbd),
-                    border: Border(bottom: BorderSide(
-                      color: Colors.lightBlue,
-                      width: 2,
-                    ),
-                    ),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                     // Image.asset("images/tourist.jpg",height: 35,),const SizedBox(height: 25,),
-                      Text("Qatar Airways",style: TextStyle(fontWeight: FontWeight.bold,
-                          color: Colors.black, fontSize: 12),),
+                onTap: ()=> launch('https://www.qatarairways.com/en-ug/homepage.html'),
+                child: Stack(
+                   // mainAxisAlignment: MainAxisAlignment.center,
+                    children:[
+                     ClipRRect(
+                borderRadius: BorderRadius.circular(15.0),
+                child:Image.asset("images/qt7.jpg",height: 250,width:250,fit: BoxFit.cover,),
+              ),
+                      const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text("Qatar Airways",style: TextStyle(fontWeight: FontWeight.bold,
+                            color: Colors.black, fontSize: 12),
+                          textAlign: TextAlign.center,),
+                      ),
                     ],
                   ),
                 ),
-              ),
+
               InkWell(
-                onTap: (){
-                 // Navigator.push(context,MaterialPageRoute(builder: (context)=>const CultureEntertainment()));
-                },
-                child: Container(
-                  decoration:const BoxDecoration(  color:Color(0xffbdbdbd),
-                    border: Border(bottom: BorderSide(
-                      color: Colors.lightBlue,
-                      width: 2,
+                onTap: ()=> launch('https://www.brusselsairlines.com/'),
+                child: Stack(
+                    //mainAxisAlignment: MainAxisAlignment.center,
+                    children:[
+                      ClipRRect(
+                      borderRadius: BorderRadius.circular(15.0),
+                      child:Image.asset("images/ba3.jpg",height: 250,width:250,fit: BoxFit.cover,),
                     ),
-                    ),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      //Image.asset("images/tourist.jpg",height: 35,),const SizedBox(height: 25,),
-                      Text("Brussels Airlines",style: TextStyle(fontWeight: FontWeight.bold,
-                          color: Colors.black, fontSize: 12),),
+                      const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text("Brussels Airlines",style: TextStyle(fontWeight: FontWeight.bold,
+                            color: Colors.black, fontSize: 12),
+                          textAlign: TextAlign.center,),
+                      ),
                     ],
                   ),
                 ),
-              ),
+
               InkWell(
-                onTap: (){
-                 // Navigator.push(context,MaterialPageRoute(builder: (context)=>CraftsShopping()));
-                },
-                child: Container(
-                  decoration:const BoxDecoration(  color:Color(0xffbdbdbd),
-                    border: Border(bottom: BorderSide(
-                      color: Colors.lightBlue,
-                      width: 2,
-                    ),
-                    ),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      //Image.asset("images/tourist.jpg",height: 35,),const SizedBox(height: 25,),
-                      Text("Ethiopia Airlines",style:  TextStyle(fontWeight: FontWeight.bold,
-                          color: Colors.black, fontSize: 12),),
+                onTap: ()=> launch('https://www.ethiopianairlines.com/aa'),
+                child: Stack(
+                   // mainAxisAlignment: MainAxisAlignment.center,
+                    children:[
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(15.0),
+                        child:Image.asset("images/ea13.jpg",height: 250,width:250,fit: BoxFit.cover,),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text("Ethiopian Airlines",style:  TextStyle(fontWeight: FontWeight.bold,
+                            color: Colors.black, fontSize: 12),
+                          textAlign: TextAlign.center,),
+                      ),
                     ],
                   ),
                 ),
-              ),
+
               InkWell(
-                onTap: (){
-                 // Navigator.push(context,MaterialPageRoute(builder: (context)=>const ServiceReviews1()));
-                },
-                child: Container(
-                  decoration:const BoxDecoration(  color:Color(0xffbdbdbd),
-                    border: Border(bottom: BorderSide(
-                      color: Colors.lightBlue,
-                      width: 2,
-                    ),
-                    ),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      //Image.asset("images/tourist.jpg",height: 35,),const SizedBox(height: 25,),
-                      Text("Turkish Airlines",style:  TextStyle(fontWeight: FontWeight.bold,
-                          color: Colors.black, fontSize: 12),),
+                onTap: ()=> launch('https://www.turkishairlines.com/'),
+                child: Stack(
+                    //mainAxisAlignment: MainAxisAlignment.center,
+                    children:[
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(15.0),
+                        child:Image.asset("images/tur3.png",height: 250,width:250,fit: BoxFit.cover,),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text("Turkish Airlines",style:  TextStyle(fontWeight: FontWeight.bold,
+                            color: Colors.black, fontSize: 12),
+                          textAlign: TextAlign.center,),
+                      ),
                     ],
                   ),
                 ),
-              ),
+
               InkWell(
-                onTap: (){
-                  //Navigator.push(context,MaterialPageRoute(builder: (context)=>const SplashScreen()));
-                },
-                child: Container(
-                  decoration:const BoxDecoration(  color:Color(0xffbdbdbd),
-                    border: Border(bottom: BorderSide(
-                      color: Colors.lightBlue,
-                      width: 2,
-                    ),
-                    ),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      //Image.asset("images/tourist.jpg",height: 35,),const SizedBox(height: 25,),
-                      Text("South African Airways",style: TextStyle(fontWeight: FontWeight.bold,
-                          color: Colors.black, fontSize: 12),),
+                onTap: ()=> launch('https://www.flysaa.com/'),
+                child: Stack(
+                    //mainAxisAlignment: MainAxisAlignment.center,
+                    children:[
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(15.0),
+                        child:Image.asset("images/sth1.jpg",height: 250,width:250,fit: BoxFit.cover,),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text("South African Airways",style: TextStyle(fontWeight: FontWeight.bold,
+                            color: Colors.black, fontSize: 12),
+                          textAlign: TextAlign.center,),
+                      ),
                     ],
                   ),
                 ),
-              ),
             ],
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,mainAxisSpacing: 10,crossAxisSpacing: 10),
             ),
           ),
         ),
-      ),
     );
   }
 }
